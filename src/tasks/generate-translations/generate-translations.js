219c70034e771e16edf67ec1d760b781b833479c
Ciprian Mocanu <ciprian@mbe.ro>
2017-10-18 16:12:44 +0200

    Fixed a bug that was causing bundles to be added to the root

--- src/tasks/generate-translations/generate-translations.js
+++ src/tasks/generate-translations/generate-translations.js
@@ -2,6 +2,7 @@ const cmd = require('../shared/cmd');
 const webpackRun = require('../webpack/webpack-run');
 const getConfig = require('../shared/get-config');
 const copyPath = require('../copy/copy-path');
+const getPlugins = require('../../plugins/get-plugins');
 
 /**
  * Builds the final file (for production)
@@ -18,17 +19,28 @@ module.exports = (options = {}, callback) => {
 		translations: true,
 	};
 
+	// Let's get all the plugins
+	const plugins = getPlugins();
+	const message = 'Succesfully wrote the translations';
+
 	if (configuration.translations)
 		webpackRun(translationOptions, (error) => {
 			if (!error)
-				// Copy to target
-				copyPath(configuration.outputPath, configuration.destination, configuration.source, options.silent, () => {
-					// Log output
-					cmd.success('Succesfully wrote the translations');
+				if ((!options.exclude || options.exclude.indexOf('copy') < 0) && plugins.copy)
+					// Copy to target
+					copyPath(configuration.outputPath, plugins.copy.config.destination, plugins.copy.config.source, options.silent, () => {
+						// Log output
+						cmd.success(message);
+
+						if (callback)
+							callback();
+					});
+				else {
+					cmd.success(message);
 
 					if (callback)
 						callback();
-				});
+				}
 		});
 	else
 		callback();
